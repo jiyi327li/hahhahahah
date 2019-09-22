ced459df413c3f59c4ed7ade5aa3795895568c51
jiyi <jiyi327li@163.com>
2019-09-22 19:10:31 +0800

    提交

---
+++ tianlong/src/main/java/com/tiasnlong/Demo.java
@@ -0,0 +1,13 @@
+package com.tiasnlong;
+
+/**
+ * @Author : tianlong
+ * @ClassName :Demo
+ * @Date 2019/9/22
+ * @Description TODO
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈");
+    }
+}
